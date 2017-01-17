46c78c690a1bb27aa34cf1b25f0208181339d2b8
Corey Sauer <csauer42@gmail.com>
2017-01-16 22:56:58 -0700

    Serial read structural changes and notes

--- avr/SerialPwmTest/SerialPwmTest/main.c
+++ avr/SerialPwmTest/SerialPwmTest/main.c
@@ -8,7 +8,8 @@
 #define F_CPU 8000000UL
 #define BAUDRATE 9600
 #define BAUD_PRESCALLER (((F_CPU / (BAUDRATE * 16UL))) - 1)
-#define CLENGTH 6
+#define CLENGTH 9
+#define START 0xFF
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -19,13 +20,12 @@ unsigned char USART_receive();
 void PWMInit();
 void setPWM(unsigned char[]);
 void clearCommand(unsigned char[]);
+int CRCcheck(unsigned char[]);
 
 int main(void)
 {
 	unsigned char command[CLENGTH];
 	int cbuff;
-	unsigned short retval = 0;
-	int i = 0;
 	clearCommand(command);
     PWMInit();
 	setPWM(command);
@@ -34,17 +34,34 @@ int main(void)
     while (1) 
     {
 		cbuff = USART_receive();
-		command[i] = cbuff;
-		retval += cbuff;
-		i++;
-		if (i == 5) {
-			setPWM(command);
-			_delay_ms(20);
-			USART_send(retval >> 8);
-			USART_send(retval & 0xFF);
-			clearCommand(command);
-			retval = 0;
-			i = 0;
+		if (cbuff == START) {
+			command[0] = START;
+			int count = 1;
+			int sum = 0;
+			do {
+				cbuff = USART_receive();
+				command[count] = cbuff;
+				sum += cbuff;
+				count++;
+				if (count == 8) {
+					if (CRCcheck(command) == 1) {
+						setPWM(command);
+						for (int i = 0; i < CLENGTH; i++) {
+							USART_send(command[i]);
+						}
+						clearCommand(command);
+						//USART_send(sum >> 8);
+						//USART_send(sum & 0xFF);
+						break;
+					} else {
+						for (int i = 0; i < CLENGTH; i++) {
+							USART_send(0xFF);
+							//USART_send(0xFF);
+						}
+						break;
+					}
+				}
+			} while ((count < 8) && (cbuff != START));
 		}
 	}
 }
@@ -62,6 +79,12 @@ void USART_send(unsigned char data) {
 }
 
 unsigned char USART_receive() {
+	// change to receive all
+	// block until data
+	// read until not data
+	// read into buffer
+	// read buffer for start bytes into secondary command buffer
+	// execute commands
 	while(!(UCSR0A & (1<<RXC0)));
 	return UDR0;
 }
@@ -95,10 +118,23 @@ void clearCommand(unsigned char c[]) {
 }
 
 void setPWM(unsigned char c[]) {
-	OCR0A = c[0];
-	OCR0B = c[1];
-	OCR1A = c[2];
-	OCR1B = c[3];
-	OCR2A = c[4];
-	OCR2B = c[5];
+	OCR0A = c[1];
+	OCR0B = c[2];
+	OCR1A = c[3];
+	OCR1B = c[4];
+	OCR2A = c[5];
+	OCR2B = c[6];
+}
+
+int CRCcheck(unsigned char c[]) {
+	uint16_t crc = 0x0000;
+	// this is a fake crc
+	for (int i = 1; i < (CLENGTH-2); i++) {
+		//crc ^= (c[i] << i);
+		crc += c[i];
+	}
+	//if (((crc >> 8) == c[7]) && ((crc & 0xFF) == c[8])) {
+	return 1;
+	//}
+    //return 0;
 }
