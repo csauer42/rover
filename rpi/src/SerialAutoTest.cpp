0e4d7357e71edc4030e88ea2b08b2249f102fd34
Corey Sauer <csauer42@gmail.com>
2017-01-17 05:54:49 +0000

    updating SerialAutoTest

--- rpi/src/SerialAutoTest.cpp
+++ rpi/src/SerialAutoTest.cpp
@@ -8,9 +8,12 @@
 
 #include <iostream>
 #include <ctime>
+#include <cstdio>
+#include <iomanip>
 #include "serial_setup.h"
 
 void writeValue(uint8_t, int);
+void writeCRC(unsigned char c[]); 
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -69,7 +72,8 @@ int main(int argc, char** argv) {
         writeValue(seed, fd);
 	seed += 0x10;
 	if (seed == 0xF0) seed = 0x10;
-        sleep(2);
+        //sleep(1);
+        usleep(250000);
     }
     // end main process
 
@@ -89,32 +93,63 @@ int main(int argc, char** argv) {
 void writeValue(uint8_t seed, int fd) {
     int rc;
     short int sum = 0;
-    unsigned char bytes[6];
-    memset(bytes, 0, 6);
+    unsigned char bytes[9];
+    memset(bytes, 0, 9);
 
-    bytes[0] = seed;
-    bytes[1] = seed + 0x10;
-    bytes[2] = seed + 0x20;
-    bytes[3] = seed + 0x30;
-    bytes[4] = seed + 0x40;
-    bytes[5] = seed + 0x50;
+    bytes[0] = 0xFF;
+    bytes[1] = seed;
+    bytes[2] = seed + 0x10;
+    bytes[3] = seed + 0x20;
+    bytes[4] = seed + 0x30;
+    bytes[5] = seed + 0x40;
+    bytes[6] = seed + 0x50;
 
-    for (int i = 0; i < 6; i++) {
+    for (int i = 1; i < 7; i++) {
         sum += bytes[i];
     }
+    writeCRC(bytes);
 
-    rc = write(fd, bytes, 6);
+    rc = write(fd, bytes, 9);
     if (rc < 0) {
         std::cerr << "Error on write: " << rc << std::endl;
     } else {
-        std::cout << "Write success: " << rc << std::endl;
+        std::cout << "Write success: " << rc << " | " << (int)((bytes[7] << 8) | bytes[8]) 
+                  << " | " << std::hex << (int)bytes[7] << " | " << std::hex << (int)bytes[8] 
+                  << std::dec << std::endl;
+        for (int i = 0; i < 9; i++) {
+            printf("%02X ", bytes[i]);            
+        }
+        printf("\n");
     }
     tcdrain(fd);
+    memset(bytes, 0, 9);
     std::cout << "Waiting for response..." << std::endl;
-    rc = read(fd, bytes, 2);
-    if (rc >= 1) {
-        std::cout << "Reply: " << (int)((bytes[0] << 8) | bytes[1]) << " Expected reply: " << sum << std::endl;
+    rc = read(fd, bytes, 9);
+    if (rc == 9) {
+        //std::cout << "Reply: " << (int)((bytes[0] << 8) | bytes[1]) << " Expected reply: " 
+        //          << sum << std::endl << std::endl;
+        std::cout << "Reply:" << std::endl;
+        std::cout << std::hex; 
+        for (int i = 0; i < 9; i++) {
+            std::cout << std::uppercase << std::setfill('0') << std::setw(2) << (int)bytes[i] << " ";
+        }
+        std::cout << std::dec << std::endl << std::endl;
     } else {
         std::cerr << "Error on read: " << rc << std::endl;
+        std::cout << std::hex;
+        for (int i = 0; i < rc; i++) {
+            std::cout << std::uppercase << std::setfill('0') << std::setw(2) << (int)bytes[i] << " ";
+        }
+        std::cout << std::dec << std::endl << std::endl;
     }
+}
+
+void writeCRC(unsigned char c[]) {
+    uint16_t crc = 0x0000;
+    for (int i=1; i < 7; i++) {
+        //crc ^= (c[i] << i);
+        crc += c[i];
+    }
+    c[7] = (crc >> 8);
+    c[8] = (crc & 0xFF);
 }
