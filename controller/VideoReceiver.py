9da0666207329efe4e5ce2b3984067a7d1f2ce75
Corey Sauer <csauer42@gmail.com>
2017-05-29 16:45:04 -0600

    VideoReceiver.py cleanup

--- controller/VideoReceiver.py
+++ controller/VideoReceiver.py
@@ -6,27 +6,28 @@ import numpy as np
 
 HOST = '192.168.0.104'
 PORT = 5001
+BUFF_SIZE = 1024
 
-vbytes = b''
+buff = b''
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST,PORT))
 
-vbytes += client.recv(2048)
+buff += client.recv(BUFF_SIZE)
 
 while True:
-    a = vbytes.find(b'\xff\xd8')
-    b = vbytes.find(b'\xff\xd9')
-    if a != -1 and b != -1:
-        jpg = vbytes[a:b+2]
-        vbytes = vbytes[b+2:]
+    start = buff.find(b'\xff\xd8')
+    stop = buff.find(b'\xff\xd9')
+    if start != -1 and stop != -1:
+        frame = buff[start:stop+2]
+        buff = buff[stop+2:]
 
-        i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-        cv2.imshow('frame', i)
+        image = cv2.imdecode(np.fromstring(frame, dtype=np.uint8), cv2.IMREAD_COLOR)
+        cv2.imshow('frame', image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         try:
-            vbytes += client.recv(2048)
+            buff += client.recv(BUFF_SIZE)
         except KeyboardInterrupt:
             break
 
